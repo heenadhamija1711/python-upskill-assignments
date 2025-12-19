0bf3e0aaf8d089915e94484cdb55ba1ed1d7d4ac
Lovee Dhamija <loveedhamija@Lovees-MacBook-Pro.local>
2025-12-19 11:34:19 +0530

    First commit

---
+++ week_1/data-type.py
@@ -0,0 +1,18 @@
+# Task: Convert the following values to the specified types and print the results
+# Convert 3.75 to an integer and print the value
+
+num=3.75
+print(int(num))
+
+
+# Convert "123" to a float and print the value
+num=123
+print(float(num))
+
+# Convert 0 to a boolean and print the value
+
+num=0
+print(bool(num))
+
+# Convert False to a string and print the value
+
